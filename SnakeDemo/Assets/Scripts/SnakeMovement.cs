e3305bbdbdfa0c2f7bc209eddb4f8be66808f7e6
Metin Kaim <metinkaim0@gmail.com>
2023-04-16 02:55:27 +0300

    Hareket bug'ı
    *Zıt yönde hareket etme bug'ı  düzeltildi.

--- SnakeDemo/Assets/Scripts/SnakeMovement.cs
+++ SnakeDemo/Assets/Scripts/SnakeMovement.cs
@@ -6,31 +6,32 @@ public class SnakeMovement : MonoBehaviour
     [SerializeField, Range(.1f, 2f)] float _waitToMove;
     [SerializeField] Transform _snakeTail;
 
-    Vector2 _moveDirectin;
+    Vector2 _moveDirection;
+    Vector2 _tempDirection;
 
     private void Start()
     {
-        _moveDirectin = Vector2.down;
+        _tempDirection = Vector2.down;
         StartCoroutine(SnakeMove());
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && _moveDirectin != Vector2.right)
+        if (Input.GetKeyDown(KeyCode.A) && _moveDirection != Vector2.right)
         {
-            _moveDirectin = Vector2.left;
+            _tempDirection = Vector2.left;
         }
-        else if (Input.GetKeyDown(KeyCode.D) && _moveDirectin != Vector2.left)
+        else if (Input.GetKeyDown(KeyCode.D) && _moveDirection != Vector2.left)
         {
-            _moveDirectin = Vector2.right;
+            _tempDirection = Vector2.right;
         }
-        else if (Input.GetKeyDown(KeyCode.W) && _moveDirectin != Vector2.down)
+        else if (Input.GetKeyDown(KeyCode.W) && _moveDirection != Vector2.down)
         {
-            _moveDirectin = Vector2.up;
+            _tempDirection = Vector2.up;
         }
-        else if (Input.GetKeyDown(KeyCode.S) && _moveDirectin != Vector2.up)
+        else if (Input.GetKeyDown(KeyCode.S) && _moveDirection != Vector2.up)
         {
-            _moveDirectin = Vector2.down;
+            _tempDirection = Vector2.down;
         }
     }
 
@@ -40,8 +41,8 @@ public class SnakeMovement : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(_waitToMove);
-
-            transform.position += (Vector3)_moveDirectin;
+            _moveDirection = _tempDirection;
+            transform.position += (Vector3)_moveDirection;
         }
 
     }
